4465c4dd0bd99957f21b56227c47c3bf58a64cc3
siddharthc <siddharthc@mindfiresolutions.com>
2022-04-10 12:10:21 +0530

    match list

--- src/components/home.js
+++ src/components/home.js
@@ -1,113 +1,114 @@
-import React, { useState } from 'react'
-import { ethers } from 'ethers'
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
-import DAIABI from '../abis/DAI.json'
-import BetABI from '../abis/Bet.json'
-import BetOracleABI from '../abis/BetOracle.json'
-
+import DAIABI from "../abis/DAI.json";
+import BetABI from "../abis/Bet.json";
+import BetOracleABI from "../abis/BetOracle.json";
 
 const Home = () => {
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [defaultAccount, setDefaultAccount] = useState(null)
-  const [userBalance, setUserBalance] = useState(null)
-  const [userDAIbalance, setuserDAIbalance] = useState(null)
-  const [connButtonText, setConnButtonText] = useState('Connect Wallet')
-  const [allBettableEvents, setAllBettableEvents] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [userDAIbalance, setuserDAIbalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const [allBettableEvents, setAllBettableEvents] = useState([]);
+  const [eventDetail, setEventDetail] = useState([]);
 
-  const DAIContractAddress = process.env.REACT_APP_DAI_CONTRACT_ADDRESS
-  const BetContractAddress = process.env.REACT_APP_Bet_CONTRACT_ADDRESS
-  const BetOracleContractAddress = process.env.REACT_APP_BetOracle_CONTRACT_ADDRESS
+  const DAIContractAddress = process.env.REACT_APP_DAI_CONTRACT_ADDRESS;
+  const BetContractAddress = process.env.REACT_APP_Bet_CONTRACT_ADDRESS;
+  const BetOracleContractAddress =
+    process.env.REACT_APP_BetOracle_CONTRACT_ADDRESS;
 
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
-  const signer = provider.getSigner()
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  useEffect(() => {
+    showBettableEvents();
+    for (let i = 0; i < allBettableEvents.length; i++) {
+      showEventDetails(allBettableEvents[i]);
+    }
+  }, [defaultAccount]);
 
   const connectWalletHandler = async () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
-      console.log('MetaMask Here!')
+      console.log("MetaMask Here!");
 
       window.ethereum
-        .request({ method: 'eth_requestAccounts' })
+        .request({ method: "eth_requestAccounts" })
         .then((result) => {
-          setDefaultAccount(result[0])
-          setConnButtonText('Wallet Connected')
-          getAccountBalance(result[0])
-          showBalance()
-          showBettableEvents()
+          setDefaultAccount(result[0]);
+          setConnButtonText("Wallet Connected");
+          getAccountBalance(result[0]);
+          showBalance();
+          showBettableEvents();
         })
         .catch((error) => {
-          setErrorMessage(error.message)
-        })
+          setErrorMessage(error.message);
+        });
     } else {
-      console.log('Need to install MetaMask')
-      setErrorMessage('Please install MetaMask browser extension to interact')
+      console.log("Need to install MetaMask");
+      setErrorMessage("Please install MetaMask browser extension to interact");
     }
-  }
+  };
 
   // update account, will cause reload the page
   const accountChangedHandler = () => {
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   const getAccountBalance = (account) => {
     window.ethereum
-      .request({ method: 'eth_getBalance', params: [account, 'latest'] })
+      .request({ method: "eth_getBalance", params: [account, "latest"] })
       .then((balance) => {
-        setUserBalance(ethers.utils.formatEther(balance))
+        setUserBalance(ethers.utils.formatEther(balance));
       })
       .catch((error) => {
-        setErrorMessage(error.message)
-      })
-  }
+        setErrorMessage(error.message);
+      });
+  };
 
   const chainChangedHandler = () => {
     // reload the page to avoid any errors with chain change mid use of application
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   const showBalance = async () => {
-    await provider.send('eth_requestAccounts', [])
-    const signerAddress = await signer.getAddress()
-    const daiContract = new ethers.Contract(DAIContractAddress, DAIABI, signer)
-    const balance = await daiContract.balanceOf(signerAddress)
-    setuserDAIbalance(ethers.utils.formatEther(balance))
-    console.log('Balance:', balance)
-  }
+    await provider.send("eth_requestAccounts", []);
+    const signerAddress = await signer.getAddress();
+    const daiContract = new ethers.Contract(DAIContractAddress, DAIABI, signer);
+    const balance = await daiContract.balanceOf(signerAddress);
+    setuserDAIbalance(ethers.utils.formatEther(balance));
+    console.log("Balance:", balance);
+  };
 
   const showBettableEvents = async () => {
-    const betContract = new ethers.Contract(
-      BetContractAddress,
-      BetABI,
-      signer
-    )
+    const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
     const betOracleContract = new ethers.Contract(
       BetOracleContractAddress,
       BetOracleABI,
       signer
-    )
-    const events = await betContract.getBettableEvents()
-    console.log('Events:', events)
-    setAllBettableEvents(events)
-  }
+    );
+    const events = await betContract.getBettableEvents();
+    //console.log("Events:", events);
+    setAllBettableEvents(events);
+  };
 
   const showEventDetails = async (eventId) => {
-    const betContract = new ethers.Contract(
-      BetContractAddress,
-      BetABI,
-      signer
-    )
+    const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
     const betOracleContract = new ethers.Contract(
       BetOracleContractAddress,
       BetOracleABI,
       signer
-    )
-    const event = await betContract.getEvent(eventId)
-    console.log('Event:', event)
-  }  
+    );
+    const event = await betContract.getEvent(eventId);
+    //setArray(oldArray => [...oldArray,newValue] );
+    setEventDetail(eventDetail => [...eventDetail, event]);
+  };
 
   // listen for account changes
-  window.ethereum.on('accountsChanged', accountChangedHandler)
+  window.ethereum.on("accountsChanged", accountChangedHandler);
 
-  window.ethereum.on('chainChanged', chainChangedHandler)
+  window.ethereum.on("chainChanged", chainChangedHandler);
 
   return (
     <div className="home">
@@ -126,30 +127,32 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="eventsDisplay">
+      <div>
         <h3>Bettable Events</h3>
         <ul>
-          {allBettableEvents.map((event) => {
-            return (
-              <li key={event}> 
-                <h3>{event} </h3>
-                <button onClick={() => showEventDetails(event)}>
-                Click here to get details
-                  {event.name}
-                  {event.id}
-                  {event.date}
-                  {event.teamAname}
-                  {event.teamBname}
-                  {event.outcome}
-                  {event.winner}
-                </button>
-              </li>
-            )
-          })}
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Category</th>
+                <th scope="col">Teams</th>
+                <th scope="col">Bet</th>
+              </tr>
+            </thead>
+            <tbody>
+   {eventDetail.map(e =>{
+     return(
+      <tr key={e.eventId}>
+     
+      <td>{e.name}</td>
+      <td>{e.teamAname} VS {e.teamBname}</td>
+      <td><button className="btn btn-success">Bet</button></td>
+    </tr>
+     )
+   })}
+    </tbody>
+          </table>
         </ul>
       </div>
-
-
     </div>
   );
 };
