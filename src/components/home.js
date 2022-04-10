83224f4b8eff13462455d45f5ac5415235087b55
sunny <itssunny322@gmail.com>
2022-04-10 08:32:23 +0530

    added bettable events list

--- src/components/home.js
+++ src/components/home.js
@@ -12,10 +12,14 @@ const Home = () => {
   const [userBalance, setUserBalance] = useState(null)
   const [userDAIbalance, setuserDAIbalance] = useState(null)
   const [connButtonText, setConnButtonText] = useState('Connect Wallet')
+  const [allBettableEvents, setAllBettableEvents] = useState([])
+
   const DAIContractAddress = process.env.REACT_APP_DAI_CONTRACT_ADDRESS
   const BetContractAddress = process.env.REACT_APP_Bet_CONTRACT_ADDRESS
   const BetOracleContractAddress = process.env.REACT_APP_BetOracle_CONTRACT_ADDRESS
 
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
 
   const connectWalletHandler = async () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
@@ -28,6 +32,7 @@ const Home = () => {
           setConnButtonText('Wallet Connected')
           getAccountBalance(result[0])
           showBalance()
+          showBettableEvents()
         })
         .catch((error) => {
           setErrorMessage(error.message)
@@ -60,19 +65,46 @@ const Home = () => {
   }
 
   const showBalance = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
     await provider.send('eth_requestAccounts', [])
-    const signer = provider.getSigner()
     const signerAddress = await signer.getAddress()
-    console.log('Account:', signerAddress)
     const daiContract = new ethers.Contract(DAIContractAddress, DAIABI, signer)
     const balance = await daiContract.balanceOf(signerAddress)
     setuserDAIbalance(ethers.utils.formatEther(balance))
     console.log('Balance:', balance)
   }
 
-  console.log(showBalance)
-  
+  const showBettableEvents = async () => {
+    const betContract = new ethers.Contract(
+      BetContractAddress,
+      BetABI,
+      signer
+    )
+    const betOracleContract = new ethers.Contract(
+      BetOracleContractAddress,
+      BetOracleABI,
+      signer
+    )
+    const events = await betContract.getBettableEvents()
+    console.log('Events:', events)
+    setAllBettableEvents(events)
+  }
+
+  const showEventDetails = async (eventId) => {
+    const betContract = new ethers.Contract(
+      BetContractAddress,
+      BetABI,
+      signer
+    )
+    const betOracleContract = new ethers.Contract(
+      BetOracleContractAddress,
+      BetOracleABI,
+      signer
+    )
+    const event = await betContract.getEvent(eventId)
+    const eventDetails = await betOracleContract.getEventDetails(eventId)
+    console.log('Event:', event)
+    console.log('Event Details:', eventDetails)
+  }  
 
   // listen for account changes
   window.ethereum.on('accountsChanged', accountChangedHandler)
@@ -82,6 +114,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Circle Bet</h1>
+
       <div className="walletCard">
         <button onClick={connectWalletHandler}>{connButtonText}</button>
         <div className="accountDisplay">
@@ -94,6 +127,30 @@ const Home = () => {
           <h3>Balance (DAI): {userDAIbalance}</h3>
         </div>
       </div>
+
+      <div className="eventsDisplay">
+        <h3>Bettable Events</h3>
+        <ul>
+          {allBettableEvents.map((event) => {
+            return (
+              <li key={event.id}>
+                <button onClick={() => showEventDetails(event.id)}>
+                  {event.name}
+                  {event.id}
+                  {event.date}
+                  {event.teamAname}
+                  {event.teamBname}
+                  {event.outcome}
+                  {event.winner}
+                </button>
+
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+
+
     </div>
   );
 };
