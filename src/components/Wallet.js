ce80bc841d1520d6d31a93878975e35cee177df4
sunny <itssunny322@gmail.com>
2022-04-11 16:12:08 +0530

    added bettable events list

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -8,14 +8,20 @@ import BetOracleABI from "../abis/BetOracle.json";
 
 export default function () {
     const [amount,setamount]=useState(0)
-    const [acountDetail,setaccountDetail]= useState()
+    const [acountDetail,setaccountDetail]= useState([])
+    const [depositedAmount,setDepositedAmount]=useState(0)
+    const [balanceLost,setBalanceLost]=useState(0)
+    const [balanceAvailable,setBalanceAvailable]=useState(0)
+    const [balanceWithdrawn,setBalanceWithdrawn]=useState(0)
+    const [ongoingBetAmount,setOngoingBetAmount]=useState(0)
+
     const factor = 1000000000000000000;
 
     const DAIContractAddress = process.env.REACT_APP_DAI_CONTRACT_ADDRESS;
     const BetContractAddress = process.env.REACT_APP_Bet_CONTRACT_ADDRESS;
     const BetOracleContractAddress =
       process.env.REACT_APP_BetOracle_CONTRACT_ADDRESS;
-  
+
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     useEffect(()=>{
@@ -23,11 +29,6 @@ export default function () {
             if(localStorage.getItem("account")!== null){
                 let account = localStorage.getItem("account")
                 const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
-                const betOracleContract = new ethers.Contract(
-                  BetOracleContractAddress,
-                  BetOracleABI,
-                  signer
-                );
                 const accountDetails = await betContract.userTokenBal(account);
                 setaccountDetail(accountDetails)
             }
@@ -37,31 +38,24 @@ export default function () {
             }
         }
         getDetail();
+        setBalanceDetails();
 
     },[])
+
   const depositHandler = async (amount) => {
     let toWei = ethers.utils.formatEther(amount)
-    console.log(toWei,amount)
-    
     const daiContract = new ethers.Contract(DAIContractAddress, DAIABI, signer);
-    console.log(BetContractAddress);
-    console.log(DAIContractAddress);
     const approvetx = await daiContract.approve(BetContractAddress,amount );
     await approvetx.wait();
-    console.log("Approval:", approvetx);
 
     const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
     const tx = await betContract.deposit(DAIContractAddress, amount);
-    console.log("Deposit:", tx);
   };
+
   const WithdrawHandler = async(amount)=>{
-      console.log("withdraw", amount)
       const daiContract = new ethers.Contract(DAIContractAddress, DAIABI, signer);
-      console.log(BetContractAddress);
-      console.log(DAIContractAddress);
       const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
       const tx = await betContract.withdrawAmount(amount);
-      console.log("Withdraw:", tx);
 
   }
   const mode=(e)=>{
@@ -73,8 +67,31 @@ export default function () {
       }
 
   }
+
+  const setBalanceDetails = async () => {
+    let account = localStorage.getItem("account")
+    const betContract = new ethers.Contract(BetContractAddress, BetABI, signer);
+    const accountDetails = await betContract.userTokenBal(account);
+    setaccountDetail(accountDetails)
+
+    if(accountDetails[0] === undefined){
+      console.log("undefined")
+  }
+  else{
+      setDepositedAmount(parseInt((accountDetails[0]._hex).slice(2), 16))
+      setBalanceAvailable(parseInt((accountDetails[1]._hex).slice(2), 16))
+      setBalanceLost(parseInt((accountDetails[2]._hex).slice(2), 16))
+      setBalanceWithdrawn(parseInt((accountDetails[3]._hex).slice(2), 16))
+      setOngoingBetAmount(parseInt((accountDetails[4]._hex).slice(2), 16))
+
+  }
+}
+
+
   return (
+
     <div className="depositCard">
+
       <h3>Deposit DAI</h3>
       <input type="number" placeholder="Amount in Wei"  onChange={(e)=>{setamount(e.target.value )}}/>
       <select onChange={(e)=>mode(e)} style={{height: 30}}>
@@ -87,21 +104,21 @@ export default function () {
       <table class="table">
   <thead>
     <tr>
-      <th scope="col">Balance Available</th>
-      <th scope="col">Balance Lost</th>
       <th scope="col">Deposited Amount</th>
+      <th scope="col">Balance Lost</th>
+      <th scope="col">Balance Available</th>
       <th scope="col">Balance Withdrawn</th>
       <th scope="col">Ongoing Bet Amount</th>
     </tr>
   </thead>
   <tbody>
-      {console.log(acountDetail)}
-    {/* <tr>
-      <th scope="row">{acountDetail.balanceAvailable}</th>
-      <td>{acountDetail.balanceLost}</td>
-      <td>{acountDetail.balanceWithdrawn}</td>
-      <td>{acountDetail.ongoingBetAmount}</td>
-    </tr> */}
+    <tr>
+      <th scope="row">{depositedAmount}</th>
+      <td>{balanceLost}</td>
+      <td>{balanceAvailable}</td>
+      <td>{balanceWithdrawn}</td>
+      <td>{ongoingBetAmount}</td>
+    </tr>
   </tbody>
 </table>
 
